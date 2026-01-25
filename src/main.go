d190aacfc0ac9992ac2f7991f6ba3216b694a01d
kaneeldias <kaneeldias@hotmail.com>
2026-01-25 10:36:09 +0530

    Remove is_active condition from inactive pages deletion query

--- src/main.go
+++ src/main.go
@@ -69,7 +69,7 @@ func HandleRequest(ctx context.Context) (Response, error) {
 
 	fmt.Printf("Deleting inactive pages older than %d days\n", deletionInterval)
 	_, err = conn.Exec(ctx, `
-		DELETE FROM pages WHERE is_active = false AND updated_at < NOW() - $1 * INTERVAL '1 day'	
+		DELETE FROM pages WHERE updated_at < NOW() - $1 * INTERVAL '1 day'	
 	`, deletionInterval)
 
 	if err != nil {
